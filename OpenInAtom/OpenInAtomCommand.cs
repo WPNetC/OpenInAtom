20a7bbeca22caebca4a2cca4a4c51f4888bf77bf
Sid <sidsoft2014@outlook.com>
2017-07-05 21:10:03 +0100

    Fix issue with opening previous project

--- OpenInAtom/OpenInAtomCommand.cs
+++ OpenInAtom/OpenInAtomCommand.cs
@@ -64,9 +64,6 @@ namespace OpenInAtom
             private set;
         }
 
-        /// <summary>
-        /// Gets the service provider from the owner package.
-        /// </summary>
         private IServiceProvider ServiceProvider
         {
             get
@@ -75,42 +72,35 @@ namespace OpenInAtom
             }
         }
 
-        /// <summary>
-        /// Initializes the singleton instance of the command.
-        /// </summary>
-        /// <param name="package">Owner package, not null.</param>
         public static void Initialize(Package package)
         {
             Instance = new OpenInAtomCommand(package);
         }
 
-        /// <summary>
-        /// This function is the callback used to execute the command when the menu item is clicked.
-        /// See the constructor to see how the menu item is associated with this function using
-        /// OleMenuCommandService service and MenuCommand class.
-        /// </summary>
-        /// <param name="sender">Event sender.</param>
-        /// <param name="e">Event args.</param>
         private void MenuItemCallback(object sender, EventArgs e)
         {
             var dte = ServiceProvider.GetService(typeof(SDTE)) as DTE;
             if (dte.SelectedItems.Count != 1)
                 return;
-
+            string fullPath = null;
             foreach (SelectedItem selectedItem in dte.SelectedItems)
             {
                 if (selectedItem.ProjectItem == null)
                     return;
 
                 var projectItem = selectedItem.ProjectItem;
-                var fullPathProperty = projectItem.Properties.Item("FullPath");
+                var fullPathProperty = projectItem?.Properties?.Item("FullPath");
                 if (fullPathProperty == null)
                     return;
-                var fullPath = fullPathProperty.Value.ToString();
 
+                fullPath = fullPathProperty.Value.ToString();
+            }
+            if (fullPath != null)
+            {
                 var proc = new System.Diagnostics.Process();
+                proc.StartInfo.UseShellExecute = false;
                 proc.StartInfo.FileName = "atom";
-                proc.StartInfo.Arguments = fullPath;
+                proc.StartInfo.Arguments = "-n \"" + fullPath + "\"";
                 proc.Start();
             }
         }
